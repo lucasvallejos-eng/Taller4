50e7e69184a338cf1f9e91cb107b41217986ec1a
lucasvallejos-eng <lucasvallejos070@fpuna.edu.py>
2024-01-21 18:28:59 -0300

    Tercera Carga

--- src/Ventanas/Principal.java
+++ src/Ventanas/Principal.java
@@ -4,6 +4,10 @@
  */
 package Ventanas;
 
+import java.awt.BorderLayout;
+
+
+
 /**
  *
  * @author lucas
@@ -13,10 +17,17 @@ public class Principal extends javax.swing.JFrame {
     /**
      * Creates new form Principal
      */
+    
+
+
     public Principal() {
         initComponents();
+        
     }
-
+    
+   
+            
+            
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -26,21 +37,122 @@ public class Principal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        FondoPrincipal = new javax.swing.JPanel();
+        BarraNavegacion = new javax.swing.JPanel();
+        OTNormal = new javax.swing.JButton();
+        OTGlobo = new javax.swing.JButton();
+        RegistroNormal = new javax.swing.JButton();
+        RegistroGlobo = new javax.swing.JButton();
+        PanelUso = new javax.swing.JPanel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        FondoPrincipal.setBackground(new java.awt.Color(255, 255, 255));
+
+        BarraNavegacion.setBackground(new java.awt.Color(153, 255, 255));
+
+        OTNormal.setText("Ot Normal");
+        OTNormal.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                OTNormalActionPerformed(evt);
+            }
+        });
+
+        OTGlobo.setText("Ot Globo");
+
+        RegistroNormal.setText("Registro");
+        RegistroNormal.setActionCommand("Registro");
+        RegistroNormal.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                RegistroNormalActionPerformed(evt);
+            }
+        });
+
+        RegistroGlobo.setText("Regsitro");
+
+        javax.swing.GroupLayout BarraNavegacionLayout = new javax.swing.GroupLayout(BarraNavegacion);
+        BarraNavegacion.setLayout(BarraNavegacionLayout);
+        BarraNavegacionLayout.setHorizontalGroup(
+            BarraNavegacionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(BarraNavegacionLayout.createSequentialGroup()
+                .addGap(18, 18, 18)
+                .addGroup(BarraNavegacionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(OTGlobo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(RegistroNormal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(OTNormal, javax.swing.GroupLayout.DEFAULT_SIZE, 163, Short.MAX_VALUE)
+                    .addComponent(RegistroGlobo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(44, Short.MAX_VALUE))
+        );
+        BarraNavegacionLayout.setVerticalGroup(
+            BarraNavegacionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(BarraNavegacionLayout.createSequentialGroup()
+                .addGap(89, 89, 89)
+                .addComponent(OTNormal)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(RegistroNormal)
+                .addGap(51, 51, 51)
+                .addComponent(OTGlobo)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(RegistroGlobo)
+                .addContainerGap(240, Short.MAX_VALUE))
+        );
+
+        PanelUso.setBackground(new java.awt.Color(0, 255, 102));
+
+        javax.swing.GroupLayout PanelUsoLayout = new javax.swing.GroupLayout(PanelUso);
+        PanelUso.setLayout(PanelUsoLayout);
+        PanelUsoLayout.setHorizontalGroup(
+            PanelUsoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 900, Short.MAX_VALUE)
+        );
+        PanelUsoLayout.setVerticalGroup(
+            PanelUsoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout FondoPrincipalLayout = new javax.swing.GroupLayout(FondoPrincipal);
+        FondoPrincipal.setLayout(FondoPrincipalLayout);
+        FondoPrincipalLayout.setHorizontalGroup(
+            FondoPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(FondoPrincipalLayout.createSequentialGroup()
+                .addComponent(BarraNavegacion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(PanelUso, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+        FondoPrincipalLayout.setVerticalGroup(
+            FondoPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(BarraNavegacion, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(PanelUso, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(FondoPrincipal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(FondoPrincipal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void OTNormalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_OTNormalActionPerformed
+        OTNormal p1=new OTNormal();
+        p1.setSize(400,400);
+        p1.setLocation(0,0);
+        
+        PanelUso.removeAll();
+        PanelUso.add(p1,BorderLayout.CENTER);
+        PanelUso.revalidate();
+        PanelUso.repaint();
+    }//GEN-LAST:event_OTNormalActionPerformed
+
+    private void RegistroNormalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RegistroNormalActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_RegistroNormalActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +190,16 @@ public class Principal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel BarraNavegacion;
+    private javax.swing.JPanel FondoPrincipal;
+    private javax.swing.JButton OTGlobo;
+    private javax.swing.JButton OTNormal;
+    private javax.swing.JPanel PanelUso;
+    private javax.swing.JButton RegistroGlobo;
+    private javax.swing.JButton RegistroNormal;
     // End of variables declaration//GEN-END:variables
+
+    private void initPanelUso() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
